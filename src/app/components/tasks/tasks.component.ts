4fee5e24afd6f02ec3455fb6ddd1ab7132fc8005
Mona Ali Mostafa <monaali980628@gmail.com>
2022-03-21 23:26:07 +0200

    show sweet alert when delete task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Task } from './../../models/task';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tasks',
@@ -26,7 +27,25 @@ export class TasksComponent implements OnInit {
   }
 
   delete(index: number): void {
-    this.tasks.splice(index, 1)
+    // this.tasks.splice(index, 1)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.tasks.splice(index, 1) //fun. to delete task
+        Swal.fire(
+          'Deleted!',
+          'Your Task has been deleted.',
+          'success'
+        )
+      }
+    })
   }
 
   getPindingTasksCount():number{
