041b1c831d128daa82bbd7eee17d89b60eda99b3
Mona Ali Mostafa <monaali980628@gmail.com>
2022-03-21 20:29:11 +0200

    add busniss logic of todolist

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,3 +1,4 @@
+import { Task } from './../../models/task';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  tasks: Task[] = [] //array of tasks
+
   constructor() { }
   ngOnInit(): void {
   }
 
+  add(title: string): void {
+    let task = new Task();  //object from task
+    task.title = title;
+    this.tasks.push(task);
+  }
+
+  update(task: Task): void {
+    task.isDone != task.isDone;
+  }
+
+  delete(index: number): void {
+    this.tasks.splice(index, 1)
+  }
+  
 }
